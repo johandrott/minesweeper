9d596888fa347bf4dc2c140f22e284b7d17da5d2
Johan Drott <118173416+johdro-albert@users.noreply.github.com>
2023-09-05 14:18:06 +0200

    chore: alternative approach

--- Minesweeper/Minefield.cs
+++ Minesweeper/Minefield.cs
@@ -1,44 +1,59 @@
 ﻿namespace Minesweeper;
 
+public class Cell
+{
+    public bool IsOpen { get; set; }
+    public bool IsBomb { get; set; }
+    public int AdjacentBombCount { get; set; }
+}
 public class Minefield
 {
-    private readonly bool[,] layoutCoordinates = new bool[5, 5];
-    private readonly bool[,] openCells = new bool[5, 5];
-    private readonly int[,] adjacentBombCount = new int[5, 5];
+    private readonly Cell[,] cells = new Cell[5, 5];
     
     private int _numberOfBombs;
     private int _numberOfOpenCells;
     public bool GameSucceeded => _numberOfOpenCells == LayoutX * LayoutY - _numberOfBombs;
     
-    public int LayoutX => layoutCoordinates.GetLength(0);
-    public int LayoutY => layoutCoordinates.GetLength(1);
+    public int LayoutX => cells.GetLength(0);
+    public int LayoutY => cells.GetLength(1);
 
-    public bool IsCellVisited(int x, int y) => openCells[x, y];
-    public bool IsBomb(int x, int y) => layoutCoordinates[x, y];
+    public Minefield()
+    {
+        for (var x = 0; x < LayoutX; x++)
+        {
+            for (var y = 0; y < LayoutY; y++)
+            {
+                cells[x, y] = new Cell();
+            }
+        }
+    }
+
+    public bool IsCellVisited(int x, int y) => cells[x, y].IsOpen;
+    public bool IsBomb(int x, int y) => cells[x, y].IsBomb;
     
     public bool IsOutOfBounds(int x, int y) =>
         x < 0 || x >= LayoutX || y < 0 || y >= LayoutY;
     
     public void SetBomb(int x, int y)
     {
-        layoutCoordinates[x, y] = true;
+        cells[x, y].IsBomb = true;
         _numberOfBombs++;
     }
     
     public void OpenCell(int x, int y)
     {
-        openCells[x, y] = true;
+        cells[x, y].IsOpen = true;
         _numberOfOpenCells++;
     }
     
     public void IncrementAdjacentBombCount(int x, int y)
     {
-        adjacentBombCount[x, y] += 1;
+        cells[x, y].AdjacentBombCount += 1;
     }
 
     public string GetDisplayValue(int x, int y)
     {
-        var bombCount = adjacentBombCount[x, y];
+        var bombCount = cells[x, y].AdjacentBombCount;
         return bombCount > 0 ? bombCount.ToString() : IsCellVisited(x,y) ? " " : "?";
             
     }
